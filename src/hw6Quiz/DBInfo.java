6b8af5236914af401dbe6e7a6a6a2a0f50d7c6c4
PFarejowicz <pfarejowicz@gmail.com>
2015-02-24 11:08:56 -0800

    added connection to database

---
+++ src/hw6Quiz/DBInfo.java
@@ -0,0 +1,10 @@
+package hw6Quiz;
+
+public class DBInfo {
+	
+	public static final String MYSQL_USERNAME = "ccs108pfarejow";
+	public static final String MYSQL_PASSWORD = "ajaevaiz";
+	public static final String MYSQL_DATABASE_SERVER = "mysql-user.stanford.edu";
+	public static final String MYSQL_DATABASE_NAME = "c_cs108_pfarejow";
+
+}
