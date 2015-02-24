52d68dfac000b1eaf235b53d0188ea762c8f0feb
SunMi Lee <sunmee0330@gmail.com>
2015-02-24 02:41:21 -0800

    Testing SunMi's Github

--- src/hw6Quiz/Question.java
+++ src/hw6Quiz/Question.java
@@ -1,7 +1,8 @@
 package hw6Quiz;
 
 public class Question {
-	private int ques_id; 
+	private int ques_id;
+	private String test = "HI PETER";
 	
 	public Question() {
 		// TODO Auto-generated constructor stub
