72ec45cb3f5cdcc19780db2b7c5a713bf32e4ce9
PFarejowicz <pfarejowicz@gmail.com>
2015-02-24 02:47:15 -0800

    test change for SunMi

--- src/hw6Quiz/Question.java
+++ src/hw6Quiz/Question.java
@@ -2,7 +2,7 @@ package hw6Quiz;
 
 public class Question {
 	private int ques_id;
-	private String test = "HI PETER";
+	private String test = "HI SUNMI :D";
 	
 	public Question() {
 		// TODO Auto-generated constructor stub
