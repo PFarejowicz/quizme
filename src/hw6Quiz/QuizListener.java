d9a8991f88968d1215124c3f89fd2c16b667c589
PFarejowicz <pfarejowicz@gmail.com>
2015-02-24 22:18:10 -0800

    added quiz listener

---
+++ src/hw6Quiz/QuizListener.java
@@ -0,0 +1,51 @@
+package hw6Quiz;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Application Lifecycle Listener implementation class QuizListener
+ *
+ */
+@WebListener
+public class QuizListener implements ServletContextListener, HttpSessionListener {
+
+    /**
+     * Default constructor. 
+     */
+    public QuizListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
+     */
+    public void sessionCreated(HttpSessionEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
+     */
+    public void sessionDestroyed(HttpSessionEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+	
+}
