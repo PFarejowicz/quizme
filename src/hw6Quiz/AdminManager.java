c5eb66484c7a5ff10a1c40e61e574f054c051297
William Huang <whuang6@stanford.edu>
2015-02-28 23:04:54 -0800

    admin manager finished

--- src/hw6Quiz/AdminManager.java
+++ src/hw6Quiz/AdminManager.java
@@ -1,7 +1,11 @@
 package hw6Quiz;
 
+import java.security.MessageDigest;
+import java.security.SecureRandom;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Random;
 
 public class AdminManager {
 	
@@ -14,31 +18,77 @@ public class AdminManager {
 	}
 	
 	public void addAnnouncement(String announcement){
-		
+		try {
+			PreparedStatement prepStmt = con.prepareStatement("INSERT INTO announcements (description) VALUES(?)");
+			prepStmt.setString(1, announcement);
+			prepStmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void removeAnnouncement(int id){
-		
+		try {
+			PreparedStatement prepStmt = con.prepareStatement("DELETE FROM announcements WHERE announcement_id = " + id);
+			prepStmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void removeUserAccount(int id){
-		
+		try {
+			PreparedStatement prepStmt = con.prepareStatement("DELETE FROM users WHERE user_id = " + id);
+			prepStmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void clearHistoryForQuiz(int id){
-		
+		try {
+			PreparedStatement prepStmt = con.prepareStatement("DELETE FROM quiz_history WHERE quiz_id = " + id);
+			prepStmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void promoteToAdmin(int id){
-		
+		try {
+			PreparedStatement prepStmt = con.prepareStatement("UPDATE users SET admin_privilege = true WHERE user_id = " + id);
+			prepStmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public int getNumberOfUsers(){
-		return 0; // TODO
+		int count = 0;
+		try {
+			PreparedStatement prepStmt = con.prepareStatement("SELECT * FROM users");
+			rs = prepStmt.executeQuery();
+			while(rs.next()){
+				count++;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return count;
 	}
 	
 	public int getNumberOfQuizzes(){
-		return 0; // TODO
+		int count = 0;
+		try {
+			PreparedStatement prepStmt = con.prepareStatement("SELECT * FROM quiz_history");
+			rs = prepStmt.executeQuery();
+			while(rs.next()){
+				count++;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return count;
 	}
 
 }
