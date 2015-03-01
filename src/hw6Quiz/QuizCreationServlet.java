4702a55421ee2ec724b332535a80e8d58d4aacf9
PFarejowicz <pfarejowicz@gmail.com>
2015-02-28 23:37:54 -0800

    fixed attribute name

--- src/hw6Quiz/QuizCreationServlet.java
+++ src/hw6Quiz/QuizCreationServlet.java
@@ -36,7 +36,7 @@ public class QuizCreationServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		QuizManager manager = (QuizManager) getServletContext().getAttribute("user manager");
+		QuizManager quizManager = (QuizManager) getServletContext().getAttribute("quiz manager");
 		int user_id = (Integer) request.getSession().getAttribute("user id"); 
 		
 		// pull the name and password from the form data
@@ -46,7 +46,7 @@ public class QuizCreationServlet extends HttpServlet {
 		Boolean multiple_pages = request.getParameter("multiple_pages").equals("Yes");
 		Boolean immediate_correction = request.getParameter("immediate_correction").equals("Yes");
 		
-		manager.addQuiz(name, description, user_id, random_order, multiple_pages, immediate_correction);
+		quizManager.addQuiz(name, description, user_id, random_order, multiple_pages, immediate_correction);
 		RequestDispatcher dispatch = request.getRequestDispatcher("add_question.jsp");
 		dispatch.forward(request, response); 
 	}
