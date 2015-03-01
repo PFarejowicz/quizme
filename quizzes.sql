b4b55d9db41eac192b1e0cd15eb0f668eabc92fc
allenyu0915 <allenyu0915@gmail.com>
2015-02-28 22:45:00 -0800

    Created jsp file for creating quizzes
    Changed auto_correction to immediate_correction in sql

--- quizzes.sql
+++ quizzes.sql
@@ -22,7 +22,7 @@ CREATE TABLE quizzes (
     user_id INT,
     random_order BOOL,
     multiple_pages BOOL,
-    auto_correction BOOL,
+    immediate_correction BOOL,
     PRIMARY KEY (quiz_id)
 );
 
--- src/hw6Quiz/Question.java
+++ src/hw6Quiz/Question.java
@@ -2,7 +2,6 @@ package hw6Quiz;
 
 public class Question {
 	private int ques_id;
-	private String test = "HI SUNMI :D";
 	
 	public Question() {
 		// TODO Auto-generated constructor stub
