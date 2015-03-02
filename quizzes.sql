527e6ee6ae6fe59dab84741cf4d5f0b575c8a2fc
PFarejowicz <pfarejowicz@gmail.com>
2015-03-02 13:06:21 -0800

    updated database to reflect change to serialized question objects

--- quizzes.sql
+++ quizzes.sql
@@ -1,8 +1,6 @@
 USE c_cs108_pfarejow;
   
 DROP TABLE IF EXISTS users;
- -- remove table if it already exists and start from scratch
-
 CREATE TABLE users (
 	user_id INT UNSIGNED AUTO_INCREMENT,
     email VARCHAR(255),
@@ -14,7 +12,6 @@ CREATE TABLE users (
 );
 
 DROP TABLE IF EXISTS quizzes;
-
 CREATE TABLE quizzes (
 	quiz_id INT UNSIGNED AUTO_INCREMENT,
     name VARCHAR(255),
@@ -26,83 +23,43 @@ CREATE TABLE quizzes (
     PRIMARY KEY (quiz_id)
 );
 
-DROP TABLE IF EXISTS quiz_history;
+DROP TABLE IF EXISTS questions;
+CREATE TABLE questions (
+  question_id INT UNSIGNED AUTO_INCREMENT,
+  quiz_id INT,
+  question_type enum('QuestionResponse','FillInTheBlank','MultipleChoice','PictureResponse'),
+  metadata LONGBLOB,
+  PRIMARY KEY (question_id)
+);
 
-CREATE TABLE quiz_history(
+DROP TABLE IF EXISTS quiz_history;
+CREATE TABLE quiz_history (
 	quiz_id INT,
 	user_id INT,
 	score INT
 );
 
 DROP TABLE IF EXISTS friends;
-
 CREATE TABLE friends (
 	user_id_1 INT UNSIGNED,
 	user_id_2 INT UNSIGNED
 );
 
 DROP TABLE IF EXISTS friend_requests;
-
 CREATE TABLE friend_requests (
 	from_user_id INT UNSIGNED,
 	to_user_id INT UNSIGNED
 );
 
 DROP TABLE IF EXISTS messages;
-
 CREATE TABLE messages (
 	sender_id INT UNSIGNED,
 	receiver_id INT UNSIGNED,
 	data VARCHAR(255)
 );
 
-DROP TABLE IF EXISTS text_questions;
-
-CREATE TABLE text_questions (
-	question_id INT UNSIGNED AUTO_INCREMENT,
-	quiz_id INT,
-	prompt VARCHAR(255),
-	answer VARCHAR(255),
-	points INT,
-	PRIMARY KEY (question_id)
-);
-
-DROP TABLE IF EXISTS fill_blank_questions;
-
-CREATE TABLE fill_blank_questions (
-	question_id INT UNSIGNED AUTO_INCREMENT,
-	quiz_id INT,
-	prompt VARCHAR(255),
-	answer VARCHAR(255),
-	points INT,
-	PRIMARY KEY (question_id)
-);
-
-DROP TABLE IF EXISTS multiple_choice_questions;
-
-CREATE TABLE multiple_choice_questions (
-	question_id INT UNSIGNED AUTO_INCREMENT,
-	quiz_id INT,
-	prompt VARCHAR(255),
-	answer VARCHAR(255),
-	points INT,
-	PRIMARY KEY (question_id)
-);
-
-DROP TABLE IF EXISTS picture_questions;
-
-CREATE TABLE picture_questions (
-	question_id INT UNSIGNED AUTO_INCREMENT,
-	quiz_id INT,
-	img_url VARCHAR(255),
-	answer VARCHAR(255),
-	points INT,
-	PRIMARY KEY (question_id)
-);
-
 DROP TABLE IF EXISTS announcements;
-
-CREATE TABLE announcements(
+CREATE TABLE announcements (
 	announcement_id INT UNSIGNED AUTO_INCREMENT,
 	description VARCHAR(255),
 	PRIMARY KEY (announcement_id)
