6b83530de55275ea7cd08171cb4c06cc71fadf15
William Huang <whuang6@stanford.edu>
2015-03-03 00:32:38 -0800

    adding announcements works

--- src/hw6Quiz/web/AdminClearQuizHistoryServlet.java
+++ src/hw6Quiz/web/AdminClearQuizHistoryServlet.java
@@ -1,11 +1,13 @@
 package hw6Quiz.web;
 
+import hw6Quiz.manager.*;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
 
 /**
  * Servlet implementation class AdminClearQuizHistoryServlet
@@ -33,7 +35,7 @@ public class AdminClearQuizHistoryServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 	}
 
 }
